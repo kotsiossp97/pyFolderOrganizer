7c2ad1daa6feeaee3caa25bb68359bebeb7cafe1
kotsiossp97 <kotsiossp@gmail.com>
2023-05-28 16:00:10 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,34 +1,43 @@
-# Configuration file for the Sphinx documentation builder.
+"""Document configuration."""
 #
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# PyModbus documentation build configuration file,
+#
+# This file is execfile()d with the current directory set to its
+# containing dir.
+#
+# pylint: skip-file
+import os
+import sys
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os, sys
-
-sys.path.insert(0, os.path.abspath('..'))
-
-import pyFolderOrganizer
-project = 'pyFolderOrganizer'
-copyright = '2023, Konstantinos Andreou'
-author = 'Konstantinos Andreou'
-version = pyFolderOrganizer.VERSION
-release = pyFolderOrganizer.VERSION
-pygments_style = 'sphinx'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = [ 'sphinx.ext.autodoc']
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+from pyFolderOrganizer import VERSION as pyFOrg_version
 
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+parent_dir = os.path.abspath(os.pardir)
+sys.path.insert(0, parent_dir)
+sys.path.append(os.path.join(parent_dir, "examples"))
+github_doc_root = "https://github.com/kotsiossp97/pyFolderOrganizer/tree/master/doc/"
 
-html_theme = 'alabaster'
-# html_theme = 'classic'
-html_static_path = ['_static']
+# -- General configuration ------------------------------------------------
+extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme", "sphinx.ext.autosectionlabel"]
+source_suffix = [".rst"]
+master_doc = "index"
+project = "pyFolderOrganizer"
+copyright = "See license"
+author = "Konstantinos Andreou"
+version = pyFOrg_version
+release = pyFOrg_version
+language = "en"
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+pygments_style = "sphinx"
+todo_include_todos = False
+
+
+# -- Options for HTML output ----------------------------------------------
+html_theme = "sphinx_rtd_theme"
+html_static_path = []
+html_sidebars = {
+    "**": [
+        "relations.html",  # needs "show_related": True theme option to display
+        "searchbox.html",
+    ]
+}
