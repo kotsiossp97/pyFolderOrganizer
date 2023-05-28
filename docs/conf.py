7c6dbc1b182e8cb4146ebaf1c498e5372e0877c9
kotsiossp97 <kotsiossp@gmail.com>
2023-05-28 17:20:55 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../pyFolderOrganizer/'))
+sys.path.insert(0, os.path.abspath('../'))
 
 project = 'pyFolderOrganizer'
 copyright = '2023, Konstantinos Andreou'
